068b5eec0075faef5d4432d2d0e8cd33bc6ac4d3
Luana <luana_limas@hotmail.com>
2020-01-24 11:18:33 -0300

    Site para pegar livro pelo email

---
+++ js/init.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDS1bGfDFONu6AP_WAj4X5YY27dDeRo66E",
+    authDomain: "livro-c1b22.firebaseapp.com",
+    databaseURL: "https://livro-c1b22.firebaseio.com",
+    projectId: "livro-c1b22",
+    storageBucket: "livro-c1b22.appspot.com",
+    messagingSenderId: "366601719364",
+    appId: "1:366601719364:web:88b6d81e16ed97a097f611"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
